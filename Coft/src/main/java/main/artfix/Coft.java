c0ea65f9868a194654c15fbf70405a95dbf950ca
Arthur <125185500+ArtFixProg@users.noreply.github.com>
2023-02-16 19:19:14 +0400

    V1.2
    Added scanner for system in from terminal and alternative for sout.

--- Coft/src/main/java/main/artfix/Coft.java
+++ Coft/src/main/java/main/artfix/Coft.java
@@ -1,9 +1,12 @@
 package main.artfix;
 
+import java.util.Scanner;
 
 public class Coft {
 
     public static boolean resultEquals = false;
+    public static String scannerString;
+    public static int scannerInt;
 
     public static void equalsInt(int a, int b) {
         resultEquals = a == b;
@@ -11,6 +14,24 @@ public class Coft {
 
     public static void equalsString(String a, String b) {
         resultEquals = a.equals(b);
+    }
+
+    public static void outString(String a) {
+        System.out.println(a);
+    }
+
+    public static void outInt(int a) {
+        System.out.println(a);
+    }
+
+    public static void inInt(int a) {
+        Scanner scanInIntIn = new Scanner(System.in);
+        scannerInt = scanInIntIn.nextInt();
+    }
+
+    public static void inString(int a) {
+        Scanner scanInIntIn = new Scanner(System.in);
+        scannerString = scanInIntIn.nextLine();
     }
 
 
